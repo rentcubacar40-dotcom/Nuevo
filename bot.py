70d024e33aa569d758f95bda9e8df66b8475d824
rentcubacar40-dotcom <rentcubacar40@gmail.com>
2025-11-13 12:58:09 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ def send_message(chat_id, text):
     )
 
 # Webhook principal que recibe mensajes de Telegram
-@app.route("/", methods=["POST"])
+@app.route("/elielthali/eliel/v1.0", methods=["POST"])
 def webhook():
     data = request.get_json()
     if "message" in data:
