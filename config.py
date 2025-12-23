a0405aebc1208a43273d81f023d1d628ee414109
Eliel <rentcubacar40@gmail.com>
2025-12-23 03:36:39 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# =========================
+# ARCHIVO: config.py
+# =========================
+
+API_ID = 20534584
+API_HASH = "6d5b13261d2c92a9a00afc1fd613b9df"
+BOT_TOKEN = "8562042457:AAGA__pfWDMVfdslzqwnoFl4yLrAre-HJ5I"
+
+DOWNLOAD_DIR = "downloads"
+MAX_FILE_SIZE = 4 * 1024 * 1024 * 1024  # 4GB
